2cf139824e0ed891f8541fc2d351fc5ad53cc9c6
Omkar kulkarni <88308267+Omkar0114@users.noreply.github.com>
2022-01-07 08:59:26 +0530

    Create Two Sum II - Input Array Is Sorted.java

---
+++ 167. Two Sum II - Input Array Is Sorted/Two Sum II - Input Array Is Sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length -1;
+
+        while(left <=right){
+            if (numbers[left] + numbers[right] < target){
+                left++; //left = left + 1 {i.e == 0+1 =1}
+            }else if (numbers[left] + numbers[right] > target){
+                right--;  // right = right - 1;
+            }else {
+                return new int[]{ left + 1 , right + 1};
+            }
+        }
+        return null;
+    }
+}
